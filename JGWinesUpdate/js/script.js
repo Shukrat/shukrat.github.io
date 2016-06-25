ae53ac9552dc8cb2f8c67bef9019bcd93dcd94b9
Douglas Mansell <douglasamansell@gmail.com>
2016-06-25 12:51:03 -0400

    Add files via upload

--- JGWinesUpdate/js/script.js
+++ JGWinesUpdate/js/script.js
@@ -7,21 +7,28 @@
 	var menuIsOpen = false;
 	var navUL;
 	var navMenu;
+	var navMenuX;
 
 	function init(){
 		navUL = document.querySelector("#navUL");
 		navMenu = document.querySelector("#navMenu");
+		navMenuX = document.querySelector("#navMenuX");
 		
 		navMenu.onclick = mobileMenu;
+		navMenuX.onclick = mobileMenu;
 	}
 
 	function mobileMenu(){
 		if(menuIsOpen){
 			navUL.style.display = "none";
+			navMenu.style.display = "block";
+			navMenuX.style.display = "none";
 			menuIsOpen = false;
 		}
 		else if(!menuIsOpen){
 			navUL.style.display = "initial";
+			navMenu.style.display = "none";
+			navMenuX.style.display = "block";
 			menuIsOpen = true;
 		}
 	}
