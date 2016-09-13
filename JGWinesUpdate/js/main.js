4a313d2e5e5eb3fb5efe59fbc7a800cba5dd3d74
Douglas Mansell <DouglasAMansell@gmail.com>
2016-09-13 10:01:12 -0400

    Testing

---
+++ JGWinesUpdate/js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var App = App || {};
+
+App.Main = {
+  // Constants
+
+  // Variables
+  winesJSON: undefined,
+  regionsJSON: undefined,
+  tastingsJSON: undefined,
+
+  // Modules
+
+  // Methods
+  init: function(){
+    console.log("App.Main.init() called");
+    winesJSON = $.getJSON("../builderFiles/wines.json");
+    console.log(winesJSON);
+  }
+
+
+};
